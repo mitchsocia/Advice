3f43ec280c606b7061d4a0cd4bdd4a3932d53778
Mitch Socia <mitch.socia@detroitlabs.com>
2019-06-13 11:13:03 -0400

    Initial Commit

---
+++ Advice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Advice
+//
+//  Created by Mitchell Socia on 6/13/19.
+//  Copyright © 2019 Mitchell Socia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
