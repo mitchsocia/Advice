c3af5efbf959c01273e65a25d85333234040c63b
Mitch Socia <mitch.socia@detroitlabs.com>
2019-07-10 13:57:04 -0400

    Taking out print and comments

--- Advice/ViewController.swift
+++ Advice/ViewController.swift
@@ -44,7 +44,7 @@ class ViewController: UIViewController {
                         print("NO JSON")
                         return
                     }
-                    //print(yodasVersion)
+                    print(yodasVersion)
                     let normalText = yodasVersion["text"] as! String
                     print(normalText)
                     let translatedYodaSpeak = yodasVersion["translated"] as! String
@@ -71,36 +71,30 @@ class ViewController: UIViewController {
         
         let dataTask = session.dataTask(with: url) { (data, response, error) in
             
-            //JSONSerialization
-            //step 1: Top Level json (allll the json coming back)
             guard let json = try? JSONSerialization.jsonObject(with: data!, options: []) as? [String: Any] else {
                 print("no json")
                 completion(nil)
                 return
             }
             
-            //step 2: grab the first thing, which for us is some thing called a "slip" which has bits of advice in it
             guard let slipJSON = json["slip"] as? [String: Any] else {
                 print("no slip")
                 completion(nil)
                 return
             }
             
-            //step 3: gimme the value WITHIN that first thing (the slip) that has the key "advice"
             guard let advice = slipJSON["advice"] as? String else {
                 print("no advice")
                 completion(nil)
                 return
             }
             
-            //step 4: same as step 3 but gimme the value for this other key "slip_id"
             guard let slipID = slipJSON["slip_id"] as? String else {
                 print("no slip_id")
                 completion(nil)
                 return
             }
             
-            //step 5: now that we have the ingredients to make a new object, let's create one!
             let wonderfulAdvice = Advice(advice: advice, slipID: slipID)
             completion(wonderfulAdvice.advice)
             
