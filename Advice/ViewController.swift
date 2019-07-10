9c187826b94c01094badb5d8cd0ef623cc07eafe
Mitch Socia <mitch.socia@detroitlabs.com>
2019-07-10 11:19:25 -0400

    Returning JSON with the proper query, now trying to parse

--- Advice/ViewController.swift
+++ Advice/ViewController.swift
@@ -11,18 +11,45 @@ import Alamofire
 
 class ViewController: UIViewController {
     
-    var advice: Advice?
+    //var advice: Advice?
     
     @IBOutlet weak var mainLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        getAdviceMethod()
+        getAdviceMethod { (advice) in
+            self.getYoda(advice: advice!)
+        }
         
     }
     
-    func getAdviceMethod() {
+    func getYoda(advice: String) {
+        
+        //URL Components:
+        var components = URLComponents(string: "https://api.funtranslations.com/translate/yoda.json")
+        let queryItem = URLQueryItem(name: "text", value: advice)
+        components?.queryItems = [queryItem]
+        guard let url = components?.url else { return }
+        let request = URLRequest(url: url)
+        
+        //Session:
+        let session = URLSession.shared
+        
+        //Data task
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            
+            guard let yodaJSON = try? JSONSerialization.jsonObject(with: data!, options: []) else {
+                print("No Yoda JSON")
+                return
+            }
+            print(yodaJSON)
+        }
+        dataTask.resume()
+    }
+    
+    
+    func getAdviceMethod(completion: @escaping (String?) -> ()) {
         
         let session = URLSession(configuration: URLSessionConfiguration.default)
         guard let url = URL(string: "https://api.adviceslip.com/advice") else {
@@ -36,35 +63,40 @@ class ViewController: UIViewController {
             //step 1: Top Level json (allll the json coming back)
             guard let json = try? JSONSerialization.jsonObject(with: data!, options: []) as? [String: Any] else {
                 print("no json")
+                completion(nil)
                 return
             }
             
             //step 2: grab the first thing, which for us is some thing called a "slip" which has bits of advice in it
             guard let slipJSON = json["slip"] as? [String: Any] else {
                 print("no slip")
+                completion(nil)
                 return
             }
             
             //step 3: gimme the value WITHIN that first thing (the slip) that has the key "advice"
             guard let advice = slipJSON["advice"] as? String else {
                 print("no advice")
+                completion(nil)
                 return
             }
             
             //step 4: same as step 3 but gimme the value for this other key "slip_id"
             guard let slipID = slipJSON["slip_id"] as? String else {
                 print("no slip_id")
+                completion(nil)
                 return
             }
             
             //step 5: now that we have the ingredients to make a new object, let's create one!
             let wonderfulAdvice = Advice(advice: advice, slipID: slipID)
+            completion(wonderfulAdvice.advice)
             
-            DispatchQueue.main.async {
-                
-                self.mainLabel.text = wonderfulAdvice.advice
-                
-            }
+            //                                    DispatchQueue.main.async {
+            //
+            //                                        self.mainLabel.text = wonderfulAdvice.advice
+            //
+            //                                    }
             
         }
         
